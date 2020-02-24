4b803e4dbd2f46d39b35df3165ec2736dbf06b0f
Adam Wilson <adam@bastion.dev>
2020-02-24 21:39:21 +0000

    ENV Test

--- build/action-staging/entrypoint.sh
+++ build/action-staging/entrypoint.sh
@@ -4,4 +4,6 @@ echo "Running with target param: '$1'"
 echo "Running with ENV_KEY from ENV: '$ENV_KEY'"
 echo "Running with ENV_LOGIN from ENV: '$ENV_LOGIN'"
 
+npm install
+
 node ./build/build.js
--- build/build.js
+++ build/build.js
@@ -1,2 +1,4 @@
+var AWS = require('aws-sdk');
+
 console.log("Running Javascript on ENV_KEY", process.env.ENV_KEY);
 console.log("Running Javascript on ENV_LOGIN", process.env.ENV_LOGIN);
