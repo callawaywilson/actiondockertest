a9b420b09e2d5ed9b8333d3f5cb4a238414d4dfd
Adam Wilson <adam@bastion.dev>
2020-02-24 23:07:17 +0000

    Production & Staging builds

--- deploy/index.js
+++ deploy/index.js
@@ -1,8 +1,8 @@
-module.exports = (cluster, tag) => {
+module.exports = (cluster, tag='latest') => {
    try {
     console.log('ENV_LOGIN: ', process.env.ENV_LOGIN)
     console.log('ENV_KEY: ', process.env.ENV_KEY)
-    console.log('Deploying to ', cluster)
+    console.log('Deploying to ', cluster, ' tag ', tag)
    } catch (error) {
     console.error('Error deploying to ', cluster)
     process.exit(1)
