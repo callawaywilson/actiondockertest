fdbcce36a2804195ceb301aea3d0195a7f27ccbf
Adam Wilson <adam@bastion.dev>
2020-02-24 21:03:14 +0000

    ENV Test

--- build/action-staging/entrypoint.sh
+++ build/action-staging/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh -l
 
-echo "Running with ENV_KEY Param: '$1'"
+echo "Running with target param: '$1'"
 echo "Running with ENV_KEY from ENV: '$ENV_KEY'"
+echo "Running with ENV_LOGIN from ENV: '$ENV_LOGIN'"
